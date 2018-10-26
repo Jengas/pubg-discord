cd1f42f5b7c55a7de335dbcf6a9081fd3771ccf1
Jengas <timaiv2@gmail.com>
2018-10-26 22:34:29 +0200

    User config

---
+++ commands/uce.js
@@ -0,0 +1,50 @@
+exports.run = async (client, message, args) => {
+    const Discord = client.Discord;
+    const logger = client.logger;
+
+    const config = client.config;
+    const dbsql = client.dbsql;
+
+    const lang = client.lang;
+
+    if (!message.channel.permissionsFor(message.author).has("MANAGE_GUILD")) return;
+
+    var serverData = dbsql.prepare('SELECT * FROM servers WHERE serverid = ?').get(message.guild.id)
+    if (serverData == undefined) {
+        dbsql.prepare('INSERT INTO servers (language, serverid) VALUES (?, ?)').run('en', message.guild.id);
+    }
+    if (serverData.language == 'ru') {
+        var lng = lang.ru;
+    } else {
+        var lng = lang.en;
+    }
+
+
+    switch (args[0]) {
+        case "lang":
+            if (args[1] == 'en') {
+                dbsql.prepare('UPDATE users SET language = ? WHERE userid = ?').run(args[1], message.author.id);
+                message.reply('👍');
+            } else
+            if (args[1] == 'ru') {
+                dbsql.prepare('UPDATE users SET language = ? WHERE userid = ?').run(args[1], message.author.id);
+                message.reply('👍');
+            } else {
+                message.reply(lng.wrongarg);
+            }
+            break;
+        default:
+            var BCEhelpEmbed = new Discord.RichEmbed()
+                .setTitle("PUBG BOT " + lng.commands)
+                .setColor("#f7c121")
+                .setFooter("PUBG BOT", "https://i.imgur.com/O8Q7Eqq.png")
+                .setTimestamp()
+                .addField(`${lng.serverlanguage}:`, "__" + client.config.prefix + "uce lang BOTLANGUAGE__\n[" + lng.example + "``" + client.config.prefix + "uce lang ru``]", true)
+
+            await message.reply(BCEhelpEmbed);
+            break;
+
+    }
+
+    logger.info(`${message.author.tag} (${message.author.id}) - ${lng.execcmd} ${__filename.split(/[\\/]/).pop().split(".")[0]}`);
+}
