d55459dbf09897b13da5daea0836d9fdd1960c8e
Jengas <timaiv2@gmail.com>
2018-10-26 22:34:56 +0200

    New notify system

--- commands/notify.js
+++ commands/notify.js
@@ -15,19 +15,54 @@ exports.run = async (client, message, args) => {
   }
 
 
-  var nuid = dbsql.prepare("SELECT * FROM users WHERE userid=?").get(message.author.id);
-  if (nuid == undefined) {
+  var userData = dbsql.prepare("SELECT * FROM users WHERE userid=?").get(message.author.id);
+  if (userData == undefined) {
     await message.reply(`${lng.accnotlinked} **${client.config.prefix}addaccount** ${lng.tolinkaccount}`);
     return;
   }
 
-  if (nuid.notify == "0") {
-    dbsql.prepare("UPDATE users SET notify = ? WHERE userid=?").run("1", message.author.id)
-    await message.reply(lng.notify_true);
-  } else
-  if (nuid.notify == "1") {
-    dbsql.prepare("UPDATE users SET notify = ? WHERE userid=?").run("0", message.author.id)
-    await message.reply(lng.notify_false);
+  switch (args[0]) {
+    case "dm":
+      dbsql.prepare('UPDATE users SET notify = ?, notifyLocation = ? WHERE userid = ?').run('1', args[0], message.author.id);
+      await message.reply(lng.notify_dm_true);
+      break;
+    case "server":
+      if (serverData.stats_toggle == '0') {
+        await message.reply(lng.notify_server_forbid);
+        dbsql.prepare("UPDATE users SET notify = ?, notifyLocation = ? WHERE userid=?").run("1", 'dm', message.author.id)
+      } else {
+        dbsql.prepare("UPDATE users SET notify = ?, notifyLocation = ? WHERE userid=?").run("1", 'server', message.author.id)
+        await message.reply(lng.notify_server_true);
+      }
+      break;
+    case "toggle":
+      if (userData.notify == '0') {
+        if (userData.notifyLocation == 'server' && serverData.stats_toggle == '0') {
+          await message.reply(lng.notify_toggle_server_error);
+          dbsql.prepare("UPDATE users SET notify = ?, notifyLocation = ? WHERE userid = ?").run("1", 'dm', message.author.id)
+        } else {
+          dbsql.prepare("UPDATE users SET notify = ? WHERE userid = ?").run("1", message.author.id)
+          await message.reply(lng.notify_toggle_on);
+        }
+      } else
+      if (userData.notify == '1') {
+        dbsql.prepare("UPDATE users SET notify = ? WHERE userid = ?").run("0", message.author.id)
+        await message.reply(lng.notify_toggle_off);
+      }
+      break;
+    default:
+      var BCEhelpEmbed = new Discord.RichEmbed()
+        .setTitle("PUBG BOT " + lng.commands)
+        .setColor("#f7c121")
+        .setFooter("PUBG BOT", "https://i.imgur.com/O8Q7Eqq.png")
+        .setTimestamp()
+        .addField(`${lng.notify_toggle}:`, "__" + client.config.prefix + "notify toggle__\n[" + lng.example + "``" + client.config.prefix + "toggle``]", false)
+        .addBlankField()
+        .addField(`${lng.list}:`, "``dm``\n``server``", false)
+        .addField(`${lng.notify}:`, "__" + client.config.prefix + "notify LOCATION BOOLEAN__\n[" + lng.example + "``" + client.config.prefix + "notify dm true``]", false)
+
+      await message.reply(BCEhelpEmbed);
+      break;
   }
   logger.info(`${message.author.tag} (${message.author.id}) - ${lng.execcmd} ${__filename.split(/[\\/]/).pop().split(".")[0]}`);
 }
