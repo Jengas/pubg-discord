85f1a1c766076b09cdd57ed435173507329d6771
Jengas <timaiv2@gmail.com>
2018-10-26 22:51:31 +0200

    Removed old code

--- events/ready.js
+++ events/ready.js
@@ -25,82 +25,9 @@ module.exports = async (client, message) => {
   console.log('Bot is now running!');
   logger.info('Bot is now running!');
 
-  dbsql.prepare('CREATE TABLE IF NOT EXISTS users ( `userid` TEXT, `pubgUser` TEXT, `pubgServer` TEXT, `notify` TEXT DEFAULT 0, `notifyLocation` TEXT DEFAULT "DM", `notifySID` TEXT, `lastmatch` TEXT )').run();
-  dbsql.prepare('CREATE TABLE IF NOT EXISTS servers ( serverid TEXT, language TEXT DEFAULT "en", stats_channel TEXT DEFAULT "false" )').run();
+  dbsql.prepare('CREATE TABLE IF NOT EXISTS users ( `userid` TEXT, `pubgUser` TEXT, `pubgServer` TEXT, `notify` TEXT DEFAULT 0, `notifyLocation` TEXT DEFAULT "DM", `notifySID` TEXT, `lastmatch` TEXT, `language` TEXT DEFAULT "en" )').run();
+  dbsql.prepare('CREATE TABLE IF NOT EXISTS servers ( `serverid` TEXT, `language` TEXT DEFAULT "en", `stats_channel` TEXT DEFAULT null, `stats_toggle` TEXT DEFAULT "0" )').run();
 
-  // async function checkUsers() {
-  //   var notify = dbsql.prepare('SELECT * FROM users WHERE notify=?').all("1");
-
-  //   for (var i = 0, l = notify.length; i < l; i++) {
-  //     var n_uid = notify[i].userid;
-  //     var n_pubgUser = notify[i].pubgUser;
-  //     var n_pubgServer = notify[i].pubgServer;
-  //     var Player = await pubgClient.getPlayer({
-  //       name: n_pubgUser
-  //     }, n_pubgServer);
-  //     var getMatch = await Player.relationships.matches[0]
-  //     try {
-  //       var Match = await pubgClient.getMatch(getMatch.id);
-  //       var lastmatch = Match.id;
-  //       var checkmatch = dbsql.prepare('SELECT * FROM users WHERE userid=?').get(n_uid);
-  //       var oldmatch = checkmatch.lastmatch;
-  //       if (lastmatch != oldmatch) {
-  //         var userObject = getObjects(Match, '', n_pubgUser);
-  //         var username = getValues(userObject, 'name');
-  //         var winPlace = getValues(userObject, 'winPlace');
-  //         var rankPoints = getValues(userObject, 'rankPoints');
-  //         var kills = getValues(userObject, 'kills');
-  //         var totalDamage = getValues(userObject, 'damageDealt');
-  //         var timeSurvived = getValues(userObject, 'timeSurvived');
-  //         var boosts = getValues(userObject, 'boosts');
-  //         var heals = getValues(userObject, 'heals');
-  //         var assists = getValues(userObject, 'assists');
-  //         var revives = getValues(userObject, 'revives');
-  //         var vehicleDestroys = getValues(userObject, 'vehicleDestroys');
-  //         var traveledOnCar = getValues(userObject, 'rideDistance');
-  //         var traveledOnWalk = getValues(userObject, 'walkDistance');
-  //         var swimDistance = getValues(userObject, 'swimDistance');
-
-  //         var teammatesObject = getObjects(Match, 'winPlace', winPlace);
-  //         var teammatesName = getValues(teammatesObject, 'name');
-  //         const playerEmbed = new Discord.RichEmbed()
-  //           .setTitle("Latest statistics:")
-  //           .setAuthor(n_pubgUser, "https://i.imgur.com/O8Q7Eqq.png")
-  //           .setColor("#f7c121")
-  //           .setFooter("PUBG BOT", "https://i.imgur.com/O8Q7Eqq.png")
-  //           .setThumbnail("https://i.imgur.com/4NHuKRX.png")
-  //           .setTimestamp()
-  //           .addField("Last match:", `**${Match.attributes.duration}** seconds in **${Match.attributes.gameMode}** gamemode.`, false)
-  //           .addBlankField(true)
-  //           .addBlankField(true)
-  //           .addField("Gamemode:", `${Match.attributes.gameMode.toUpperCase()}`, true)
-  //           .addField("Teammates:", `${teammatesName}`.replace(/,\s?/g, "\n"), true)
-  //           .addField("Rank:", `${winPlace}`, true)
-  //           .addField("Rank Points:", `${rankPoints}`, true)
-  //           .addField("Time Survived:", `${secondsTohhmmss(Math.ceil(timeSurvived))}`, true)
-  //           .addField("Kills:", `${kills}`, true)
-  //           .addField("Assists:", `${assists} times`, true)
-  //           .addField("Total Damage:", `${totalDamage} hp`, true)
-  //           .addField("Revives:", `${revives}`, true)
-  //           .addField("Used heals:", `${heals}`, true)
-  //           .addField("Used Boosts:", `${boosts}`, true)
-  //           .addField("Traveled on car:", `${roundUp(traveledOnCar, 1)} m`, true)
-  //           .addField("Walked:", `${roundUp(traveledOnWalk, 1)} m`, true)
-  //         await client.users.get(n_uid).send(`Your latest stats on the match:`, playerEmbed);
-  //       } else {}
-  //       dbsql.prepare('UPDATE users SET lastmatch = ? WHERE userid = ?').run(lastmatch, n_uid)
-  //     } catch (e) {
-  //       dbsql.prepare("UPDATE users SET notify = ? WHERE userid=?").run("0", message.author.id)
-  //       continue;
-  //     }
-  //     await wait(2 * 1000);
-  //   }
-  // }
-
-
-  // setInterval(async function () {
-  //   checkUsers();
-  // }, client.config.pubg.notify_interval * 1000);
 
   async function asyncServerStats(serverdata, userdata) {
     if (serverdata.language == 'ru') {
@@ -1046,7 +973,7 @@ module.exports = async (client, message) => {
   setInterval(async function () {
     checkServerUsers();
     checkUsers();
-  }, 10 * 1000);
+  }, client.config.pubg.notify_interval * 1000);
 
 
 };
