a5e209cc3ba8e61679b52489b4de72757f1f5c8a
Jengas <timaiv2@gmail.com>
2018-11-01 11:54:14 +0100

    Invite link. Closes #3

---
+++ commands/invite.js
@@ -0,0 +1,27 @@
+exports.run = async (client, message, args) => {
+    const logger = client.logger;
+    const lang = client.lang;
+    const dbsql = client.dbsql;
+
+    var serverData = dbsql.prepare('SELECT * FROM servers WHERE serverid = ?').get(message.guild.id)
+    if (serverData == undefined) {
+        if (message.guild.region == 'russia') {
+            dbsql.prepare('INSERT INTO servers (serverid, language) VALUES (?, ?)').run(message.guild.id, 'ru');
+        } else {
+            dbsql.prepare('INSERT INTO servers (serverid) VALUES (?)').run(message.guild.id);
+        }
+        var serverData = dbsql.prepare('SELECT * FROM servers WHERE serverid = ?').get(message.guild.id)
+    }
+    if (serverData.language == 'ru') {
+        var lng = lang.ru;
+    } else {
+        var lng = lang.en;
+    }
+
+    try {
+        await client.users.get(message.author.id).send(`${lng.bot_invite_message} https://discordapp.com/api/oauth2/authorize?client_id=442019408446095370&permissions=68608&scope=bot`);
+    } catch (error) {
+        logger.error(error);
+    }
+
+}
