5be1f3d47556f7956ecf88c9dab17df55a33a957
Jengas <timaiv2@gmail.com>
2019-03-11 12:34:18 +0100

    notify rewrite

--- commands/notify.js
+++ commands/notify.js
@@ -2,72 +2,52 @@ exports.run = async (client, message, args) => {
   const Discord = client.Discord;
   const dbsql = client.dbsql;
   const logger = client.logger;
-  const lang = client.lang;
+  const pubgClient = client.pubgClient;
+  const PUBGservers = client.PUBGservers;
+  const genStats = client.genStats;
+  const getServerLanguage = client.getServerLanguage;
 
-  var serverData = dbsql.prepare('SELECT * FROM servers WHERE serverid = ?').get(message.guild.id)
-  if (serverData == undefined) {
-    if (message.guild.region == 'russia') {
-      dbsql.prepare('INSERT INTO servers (serverid, language) VALUES (?, ?)').run(message.guild.id, 'ru');
-    } else {
-      dbsql.prepare('INSERT INTO servers (serverid) VALUES (?)').run(message.guild.id);
-    }
-    var serverData = dbsql.prepare('SELECT * FROM servers WHERE serverid = ?').get(message.guild.id)
-  }
-  if (serverData.language) {
-    var lng = lang[serverData.language];
-  } else {
-    var lng = lang.en;
-  }
+  let lng = await getServerLanguage(message).then(l => l);
+
+  let serverData = dbsql.prepare('SELECT * FROM servers WHERE serverid = ?').get(message.guild.id)
+  let dbUser = dbsql.prepare("SELECT * FROM users WHERE userid=?").get(message.author.id);
+  if (!dbUser) return await message.reply(`Your PUBG account is not linked to your Discord account. Please proceed **${client.config.prefix}start**`);
+
+  message.reply(`Please select an option that you want to proceed.\n\n**1** - I want to recieve stats in to the **DM**\n**2** - I want to recieve stats in the **SERVER**\n**3** - I don't want to recieve stats anymore\nP.S. Enter numbers`)
+  let collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, {
+    time: 60 * 1000
+  });
 
 
-  var userData = dbsql.prepare("SELECT * FROM users WHERE userid=?").get(message.author.id);
-  if (userData == undefined) {
-    await message.reply(`${lng.accnotlinked} **${client.config.prefix}addaccount** ${lng.tolinkaccount}`);
-    return;
-  }
+  collector.on('collect', async (message) => {
 
-  switch (args[0]) {
-    case "dm":
-      dbsql.prepare('UPDATE users SET notify = ?, notifyLocation = ? WHERE userid = ?').run('1', args[0], message.author.id);
-      await message.reply(lng.notify_dm_true);
-      break;
-    case "server":
-      if (serverData.stats_toggle == '0') {
-        await message.reply(lng.notify_server_forbid);
-        dbsql.prepare("UPDATE users SET notify = ?, notifyLocation = ? WHERE userid=?").run("1", 'dm', message.author.id)
-      } else {
-        dbsql.prepare("UPDATE users SET notify = ?, notifyLocation = ? WHERE userid=?").run("1", 'server', message.author.id)
-        await message.reply(lng.notify_server_true);
-      }
-      break;
-    case "toggle":
-      if (userData.notify == '0') {
-        if (userData.notifyLocation == 'server' && serverData.stats_toggle == '0') {
-          await message.reply(lng.notify_toggle_server_error);
-          dbsql.prepare("UPDATE users SET notify = ?, notifyLocation = ? WHERE userid = ?").run("1", 'dm', message.author.id)
+    switch (message.content) {
+      case '1':
+        dbsql.prepare('UPDATE users SET notify = ?, notifyLocation = ? WHERE userid = ?').run('1', args[0], message.author.id);
+        await message.reply(`Now bot **will** notify your statistics after each match in **DM**`);
+        collector.stop();
+        break;
+      case '2':
+        if (serverData.stats_toggle == '0') {
+          await message.reply(`I was forbidden to send your statistics to this server, so instead I will send statistics to your **DM**`);
+          dbsql.prepare("UPDATE users SET notify = ?, notifyLocation = ? WHERE userid=?").run("1", 'dm', message.author.id)
         } else {
-          dbsql.prepare("UPDATE users SET notify = ? WHERE userid = ?").run("1", message.author.id)
-          await message.reply(lng.notify_toggle_on);
+          dbsql.prepare("UPDATE users SET notify = ?, notifyLocation = ? WHERE userid=?").run("1", 'server', message.author.id)
+          await message.reply(`Now I will send your statistics after each match in this **SERVER**!`);
         }
-      } else
-      if (userData.notify == '1') {
+        collector.stop();
+        break;
+      case '3':
         dbsql.prepare("UPDATE users SET notify = ? WHERE userid = ?").run("0", message.author.id)
-        await message.reply(lng.notify_toggle_off);
-      }
-      break;
-    default:
-      var BCEhelpEmbed = new Discord.RichEmbed()
-        .setTitle("PUBG BOT " + lng.commands)
-        .setColor("#f7c121")
-        .setFooter("PUBG BOT", "https://i.imgur.com/O8Q7Eqq.png")
-        .setTimestamp()
-        .addField(`${lng.notify_toggle}:`, "__" + client.config.prefix + "notify toggle__\n[" + lng.example + "``" + client.config.prefix + "notify toggle``]", false)
-        .addBlankField()
-        .addField(`${lng.list}:`, "``dm``\n``server``", false)
-        .addField(`${lng.notify}:`, "__" + client.config.prefix + "notify LOCATION BOOLEAN__\n[" + lng.example + "``" + client.config.prefix + "notify dm true``]", false)
+        await message.reply(`Now bot **will not** notify your statistics after each match`);
+        collector.stop();
+        break;
 
-      await message.reply(BCEhelpEmbed);
-      break;
-  }
-  logger.info(`${message.author.tag} (${message.author.id}) - ${lng.execcmd} ${__filename.split(/[\\/]/).pop().split(".")[0]}`);
+      default:
+        break;
+    }
+  });
+  await message.delete()
+    .then(msg => logger.info(`${message.author.tag} (${message.author.id}) - executed command ${__filename.split(/[\\/]/).pop().split(".")[0]}`))
+    .catch(console.error);
 }
