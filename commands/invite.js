08e5304551b45c1f3730e8266747e530e41fc8ad
Jengas <timaiv2@gmail.com>
2019-03-11 12:33:48 +0100

    invite rewrite

--- commands/invite.js
+++ commands/invite.js
@@ -1,22 +1,9 @@
 exports.run = async (client, message, args) => {
+    const Discord = client.Discord;
     const logger = client.logger;
-    const lang = client.lang;
-    const dbsql = client.dbsql;
+    const getServerLanguage = client.getServerLanguage;
 
-    var serverData = dbsql.prepare('SELECT * FROM servers WHERE serverid = ?').get(message.guild.id)
-    if (serverData == undefined) {
-        if (message.guild.region == 'russia') {
-            dbsql.prepare('INSERT INTO servers (serverid, language) VALUES (?, ?)').run(message.guild.id, 'ru');
-        } else {
-            dbsql.prepare('INSERT INTO servers (serverid) VALUES (?)').run(message.guild.id);
-        }
-        var serverData = dbsql.prepare('SELECT * FROM servers WHERE serverid = ?').get(message.guild.id)
-    }
-    if (serverData.language) {
-        var lng = lang[serverData.language];
-    } else {
-        var lng = lang.en;
-    }
+    let lng = await getServerLanguage(message).then(l => l);
 
     try {
         await client.users.get(message.author.id).send(`${lng.bot_invite_message} https://discordapp.com/api/oauth2/authorize?client_id=442019408446095370&permissions=68608&scope=bot`);
